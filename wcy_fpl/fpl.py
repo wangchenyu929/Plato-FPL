3aa6a01a078ac3e7bf45de8bfbf48e4c27ccf00f
wangchenyu929 <850201374@qq.com>
2023-10-09 11:06:09 +0000

    MNIST motivation exp finish

--- wcy_fpl/fpl.py
+++ wcy_fpl/fpl.py
@@ -8,7 +8,7 @@ sys.path.append("..")
 
 from plato.servers import registry as server_registry
 
-# os.environ["config_file"] = "fpl.yml"
+os.environ["config_file"] = "fpl.yml"
 # os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 # os.environ["MKL_THREADING_LAYER"] = "GNU"
 # os.environ["TOKENIZERS_PARALLELISM"] = "false"
